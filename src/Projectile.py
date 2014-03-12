472c86c7a83f29b015bd74c9024eab55f84ed4fa
MrMcKizzle <ziesunkownemail@clueless.duh>
2014-03-11 21:39:56 -0600

    fixed a variable name error

--- src/Projectile.py
+++ src/Projectile.py
@@ -11,14 +11,14 @@ LB_GRAIN = 1.0/7000.0
 GRS = "grains"
 BCF = "ballistic coefficient"
 MVL = "muzzle velocity"
-VTR = "velocity trajectory"
+VRT = "velocity trajectory"
 SRT = "short range trajectory"
 LRT = "long range trajectory"
 NME = "name"
 
 def dictionary_for_bullet(dictionary):
     """ Converts a dictionary into a bullet
-        Defaults to G1 for all bullets. 
+        Defaults to G1 for all bullets.
     """
     xy_init  = [0.0, 0.0]
     angle    = 0.065 # zero degrees
@@ -26,7 +26,7 @@ def dictionary_for_bullet(dictionary):
     bc = dictionary[BCF]
     gr = dictionary[GRS]
     projectile     = Projectile(xy_init, angle, velocity, bc, gr)
-    projectile.vtr = np.array(dictionary[VTR])
+    projectile.vrt = np.array(dictionary[VRT])
     projectile.lrt = np.array(dictionary[LRT])
     projectile.srt = np.array(dictionary[SRT])
     projectile.name = dictionary[NME]
@@ -39,7 +39,7 @@ def load_yamlfile(file_name):
     return bullets
 
 def plot_trajectories(bullet):
-    """ Plot and compare the bullet trajectory and velocity to the 
+    """ Plot and compare the bullet trajectory and velocity to the
         long range trajectory and velocity golden data.
         :param bullet: A bullet to plot.
     """
@@ -58,19 +58,19 @@ def plot_trajectories(bullet):
     title = "Velocity of a %s: %d grain Bullet and %s Model" % (bullet.name, bullet.mass, bullet.model.name)
     plt.title(title)
     plt.legend([vel, vrt], ["Model", "Data"])
- 
+
 
 def zero_in(cartridge, starting_theta, dist, tol, r):
-    """ Takes a Projectile and zeros it in the specified distance within 
-        a tolerance using a 'geometric' search algorithm. 
+    """ Takes a Projectile and zeros it in the specified distance within
+        a tolerance using a 'geometric' search algorithm.
 
         :param cartridge: the cartridge to zero in
-        :param starting_theta: the starting angle in degrees. 
+        :param starting_theta: the starting angle in degrees.
         :param dist: the distance to zero in on. Expects the distance to be yards.
-        :param tol: the tolorance 
+        :param tol: the tolorance
         :param r: the ratio (affects the speed at which the search algorithm converges)
     """
- 
+
     zeroed_in = False
     k = 1.0
     theta = starting_theta
@@ -81,7 +81,7 @@ def zero_in(cartridge, starting_theta, dist, tol, r):
         cartridge.set_fireing_angle(theta)
         cartridge.fire()
         y = cartridge.elevation_at(dist)
- 
+
         if np.abs(y) < tol:
             zeroed_in = True
 
@@ -94,43 +94,43 @@ def zero_in(cartridge, starting_theta, dist, tol, r):
 
     cartridge.reset_trajectory()
     cartridge.set_fireing_angle(theta)
-    cartridge.fire() # fire the trajectory so the user can plot right away. 
+    cartridge.fire() # fire the trajectory so the user can plot right away.
 
     return theta
 
 class Projectile:
     def __init__(self, xy_init, angle, velocity, bc, gr, sproj = bm.G1()):
         self.xy_init  =  np.array(xy_init).astype(float) # Convert array into floats.
-        
+
         # Set initial directional velocity.
         self.theta = ((float(angle)/360.0)*(2.0*np.pi))  # Convert to radians
         self.v_init = float(velocity) #fps
         self.vx_init = self.v_init*np.cos(self.theta)
         self.vy_init = self.v_init*np.sin(self.theta)
-        
+
         self.bc = float(bc) # Ballistic coefficient.
-        
-        # Simplified the balistic model setting process makes it easier to read. 
-        self.model = sproj 
-    
-        self.mass = float(gr) #* LB_GRAIN  
+
+        # Simplified the balistic model setting process makes it easier to read.
+        self.model = sproj
+
+        self.mass = float(gr) #* LB_GRAIN
         self.t = np.array([])
 
         #[x, y, vx, vy]
         # Initial Trajectory
-        self.x = [np.array([self.xy_init[0], self.xy_init[1], self.vx_init, self.vy_init])] 
+        self.x = [np.array([self.xy_init[0], self.xy_init[1], self.vx_init, self.vy_init])]
 
         # Default integration rules (seconds)
         self.t_0 = 0.0
         self.t_max = 0.5
         self.dt = 0.001
-        
+
         # Velocity Trajectory
-        self.vrt = np.array([[100, 2889], [200, 2514], [300, 2168], [400, 1851], [500, 1568]])
+        self.vrt = np.array([[100, 2889], [200, 2514], [300, 2168], [400, 1851], [500, 1568]]).astype(float)
         # Long Range Trajectory
-        self.lrt = np.array([[100., 2.0], [150., 1.7], [200., 0.], [250., -3.4], [300., -8.8], [400., -26.2], [500., -54.8]])
+        self.lrt = np.array([[100., 2.0], [150., 1.7], [200., 0.], [250., -3.4], [300., -8.8], [400., -26.2], [500., -54.8]]).astype(float)
         # Short Range Trajectory
-        self.srt = np.array([[50., 0.0], [100., 0.], [150., -1.2], [200., -3.9], [250., -8.4], [300., -14.7]])
+        self.srt = np.array([[50., 0.0], [100., 0.], [150., -1.2], [200., -3.9], [250., -8.4], [300., -14.7]]).astype(float)
 
         self.name = "Sample Cartridge"
 
@@ -139,21 +139,14 @@ class Projectile:
         v = np.array([x[2], x[3]])
         vmag = np.linalg.norm(v)
         vu = v/vmag
-        
-        f = self.model.f(vmag, self.bc) 
+
+        f = self.model.f(vmag, self.bc)
         ax = -f*(v[0])*vu[0]
         ay = -g-f*(v[1])*vu[1]
-        
-        out = np.array([v[0], v[1], ax, ay])
-        
-        return out
 
-    #def f(self, vel):
-    #    """ Drag function """
-    #    Av, Mv = self.model.get_am(vel)
-    #    force = Av / self.bc * vel**(Mv - 1) ## * 100000
-    #     
-    #    return force
+        out = np.array([v[0], v[1], ax, ay])
+
+        return out
 
     def set_integration_params(self, start_time, max_time, dt):
         """ Set the start time, stop time, and dt values that
@@ -177,7 +170,7 @@ class Projectile:
             i.integrate(i.t+self.dt)
             self.x.append(i.y)
         self.t = times
-        
+
     def plotme(self):
         x = np.array(self.x)
         pltme, = plt.plot(x[:, 0]/3., x[:, 1]*12., 'k')
@@ -186,7 +179,7 @@ class Projectile:
 
         return pltme
 
-    def plot_vel(self): 
+    def plot_vel(self):
         x = np.array(self.x)
         vels = [np.linalg.norm(x[i,2:4]) for i in range(0, x.shape[0])]
         vels = np.array(vels)
@@ -208,10 +201,10 @@ class Projectile:
     def plot_srt(self):
         plt_srt = plt.plot(self.srt[:, 0], self.srt[:, 1], '-rs')
         return plt_srt
-        
+
 
     def reset_trajectory(self):
-        """ Reset the trajectory to its initial values. 
+        """ Reset the trajectory to its initial values.
             Make sure to set the fireing angle afterwards
         """
         self.x = [np.array([self.xy_init[0], self.xy_init[1], 0.0 , 0.0])] # Initial Trajectory
@@ -225,19 +218,19 @@ class Projectile:
         self.theta = angle
 
     def set_fireing_angle(self, theta=0.06):
-        """ Sets the initial x y velocitites based on the fireing angle.  
+        """ Sets the initial x y velocitites based on the fireing angle.
             :param theta: Firing angle in degrees (def: 0.0 deg)
         """
         theta  =  np.pi / 180.0 * float(theta)  #convert to radians.
         self.x[-1][2]  =  float(self.v_init) * np.cos(float(theta)) # Set the X velocity.
         self.x[-1][3]  =  float(self.v_init) * np.sin(float(theta)) # Set the Y velocity.
-                                                                                
+
     def get_x(self):
         """ Return the trajectory """
         return self.x
 
     def elevation_at(self,  dist):
-        """ Finds the elevation at a specified distance. 
+        """ Finds the elevation at a specified distance.
             Don't forget to pass in the distance as feet
         """
         dist  =  float(dist)
@@ -252,3 +245,10 @@ class Projectile:
 
         return y
 
+    def __str__(self):
+        nm = "Name: %s\n" % self.name
+        v0 = "Muzzle Velocity: %0.2f ft/sec\n" % self.v_init
+        ms = "Grains: %0.2f\n" % self.mass
+        md = "Ballistic Model: %s\n" % self.model.__str__()
+        vt = "Velocity Trajectory (yards, ft/sec):\n%s\n" % self.vrt
+        return nm + v0 + ms + md + vt
